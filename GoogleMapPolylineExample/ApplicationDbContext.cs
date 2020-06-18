69396600da884645d377e257ece9f514b5101d8d
Patrick Wensel <pwensel@hotmail.com>
2020-06-18 09:40:28 +0300

    Initial Commit

---
+++ GoogleMapPolylineExample/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GoogleMapPolylineExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoogleMapPolylineExample
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public virtual DbSet<CorridorTrackPoint> CorridorTrackPoints { get; set; }
+    }
+}
---
+++ GoogleMapPolylineExample/Models/CorridorTrackPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleMapPolylineExample.Models
+{
+    public class CorridorTrackPoint
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+
+    }
+}
